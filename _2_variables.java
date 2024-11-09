d66884549d7f7c108e486a088ebb04478a5d4531
Syed Ibrahim <hasanibrahim48845@gmail.com>
2024-11-09 23:57:47 +0530

    Add files via upload

---
+++ _2_variables.java
@@ -0,0 +1,29 @@
+package Basics;
+
+public class _2_variables {
+    public static void main(String[] args) {
+        int x;
+        x=500;
+        System.out.println(x);
+        int z;
+        int y;
+        z=7;
+        y=7;
+        System.out.println(z+y);
+        x=400;
+        System.out.println(x);
+        System.out.println(x+1);
+        System.out.println(x+5);
+        x =x-5;
+        System.out.println(x);
+        int a = 5;
+        System.out.println(a);
+        a = 7;
+        System.out.println(a);
+        a = a+6;
+        System.out.println(a);
+        a = a-20;
+        System.out.println(a);
+
+    }
+}

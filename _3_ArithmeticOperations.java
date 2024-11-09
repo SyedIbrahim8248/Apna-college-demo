2eea53372420a621b0ad4b45ad8d076b507606a6
Syed Ibrahim <hasanibrahim48845@gmail.com>
2024-11-10 00:00:36 +0530

    Add files via upload

---
+++ _3_ArithmeticOperations.java
@@ -0,0 +1,35 @@
+package Basics;
+
+import java.sql.SQLOutput;
+
+public class _3_ArithmeticOperations {
+    public static void main(String[] args) {
+        int  x = 10;
+        int y = 3;
+        System.out.println("The value of x is " +x);
+        System.out.println("The value of y is " +y);
+        System.out.println("SYED" +x+y  );
+        System.out.println(x+y+ "Syed");
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        int a, b, c;
+        a = 5;
+        b = 6;
+        c = 7;
+        System.out.println(a+b+c);
+        System.out.println(a-b+c);
+        int p =5,q = 6,r = 7;
+        System.out.println(p);
+        System.out.println(q);
+        r = 10;
+        r = p;
+        System.out.println(r);
+        System.out.println(p+q+r);
+        System.out.println(p+100);
+        System.out.println("Hello PW " + "Hello CW");
+
+    }
+
+}

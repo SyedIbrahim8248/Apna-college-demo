204052b7376c9d6163feca5e58ecbc5cb801f829
Syed Ibrahim <hasanibrahim48845@gmail.com>
2024-11-10 00:01:09 +0530

    Add files via upload

---
+++ _5_sumof2number.java
@@ -0,0 +1,26 @@
+package Basics;
+
+public class _5_sumof2number {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 10;
+        System.out.println( x+y);
+        int sum = x + y;
+        System.out.println(sum);
+        System.out.println(x-y);
+        System.out.println(x-y+x);
+        x = 5;
+        System.out.println(x-y);
+        System.out.println(x+y);
+        y = 5;
+        System.out.println(x+y);
+        x = y;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        
+
+    }
+}
